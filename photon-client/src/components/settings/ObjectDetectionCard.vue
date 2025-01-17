1c00ace2a912dd7eba689675155a3c8795dfefec
Sam948-byte <techguy763@gmail.com>
2025-01-17 13:46:06 -0600

    in progress

--- photon-client/src/components/settings/ObjectDetectionCard.vue
+++ photon-client/src/components/settings/ObjectDetectionCard.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref, computed, inject } from "vue";
 import axios from "axios";
 import { useStateStore } from "@/stores/StateStore";
 import { useSettingsStore } from "@/stores/settings/GeneralSettingsStore";
+
+const address = inject<string>("backendHost");
 
 const showImportDialog = ref(false);
 const importRKNNFile = ref<File | null>(null);
@@ -62,6 +64,52 @@ const supportedModels = computed(() => {
   const { availableModels, supportedBackends } = useSettingsStore().general;
   return supportedBackends.flatMap((backend) => availableModels[backend] || []);
 });
+
+const exportModels = ref();
+const openExportModelsPrompt = () => {
+  exportModels.value.click();
+};
+
+const showModelsImportDialog = ref(false);
+const importFile = ref<File | null>(null);
+const handleObjectDetectionModelsImport = () => {
+  if (importFile.value === null) return;
+
+  const formData = new FormData();
+  formData.append("data", importFile.value);
+
+  axios
+    .post(`/utils/objectDetectionModelsImport`, formData, {
+      headers: { "Content-Type": "multipart/form-data" }
+    })
+    .then((response) => {
+      useStateStore().showSnackbarMessage({
+        message: response.data.text || response.data,
+        color: "success"
+      });
+    })
+    .catch((error) => {
+      if (error.response) {
+        useStateStore().showSnackbarMessage({
+          color: "error",
+          message: error.response.data.text || error.response.data
+        });
+      } else if (error.request) {
+        useStateStore().showSnackbarMessage({
+          color: "error",
+          message: "Error while trying to process the request! The backend didn't respond."
+        });
+      } else {
+        useStateStore().showSnackbarMessage({
+          color: "error",
+          message: "An error occurred while trying to process the request."
+        });
+      }
+    });
+
+  showImportDialog.value = false;
+  importFile.value = null;
+};
 </script>
 
 <template>
@@ -111,6 +159,56 @@ const supportedModels = computed(() => {
                   >
                     <v-icon left class="open-icon"> mdi-import </v-icon>
                     <span class="open-label">Import Object Detection Model</span>
+                  </v-btn>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-dialog>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-btn color="secondary" @click="openExportModelsPrompt">
+            <v-icon left class="open-icon"> mdi-export </v-icon>
+            <span class="open-label">Export Object Detection Models</span>
+          </v-btn>
+          <a
+            ref="exportModels"
+            style="color: black; text-decoration: none; display: none"
+            :href="`http://${address}/api/settings/photonvision_models.zip`"
+            download="photonvision-models.zip"
+            target="_blank"
+          />
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-btn color="secondary" @click="() => (showModelsImportDialog = true)">
+            <v-icon left class="open-icon"> mdi-import </v-icon>
+            <span class="open-label">Import Object Detection Models From Zip</span>
+          </v-btn>
+          <v-dialog
+            v-model="showModelsImportDialog"
+            width="600"
+            @input="
+              () => {
+                importFile = null;
+              }
+            "
+          >
+            <v-card color="primary" dark>
+              <v-card-title>Import Settings</v-card-title>
+              <v-card-text>
+                Upload previously exported object detection models to this device. The file should be a `.zip` file.
+                <v-row class="mt-6 ml-4 mr-8">
+                  <v-file-input v-model="importFile" :accept="'.zip'" />
+                </v-row>
+                <v-row
+                  class="mt-12 ml-8 mr-8 mb-1"
+                  style="display: flex; align-items: center; justify-content: center"
+                  align="center"
+                >
+                  <v-btn color="secondary" :disabled="importFile === null" @click="handleObjectDetectionModelsImport">
+                    <v-icon left class="open-icon"> mdi-import </v-icon>
+                    <span class="open-label">Import Object Detection Models</span>
                   </v-btn>
                 </v-row>
               </v-card-text>
